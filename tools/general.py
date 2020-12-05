6281ce9f2141aa814c556ea2127940ed00b8d933
rds504 <8250802+rds504@users.noreply.github.com>
2020-12-05 11:54:12 +0000

    Formatting

--- tools/general.py
+++ tools/general.py
@@ -1,7 +1,10 @@
 from os import path
 
 def load_input(filename):
+
     input_data = ""
+
     with open(path.join("input", filename)) as fd:
         input_data = fd.read()
+
     return input_data
