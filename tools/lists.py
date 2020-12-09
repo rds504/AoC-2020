6ccce923c6827afcea9321ce77fb33ab07ffc53f
rds504 <8250802+rds504@users.noreply.github.com>
2020-12-09 08:47:04 +0000

    Make pylint run clean on tools

--- tools/lists.py
+++ tools/lists.py
@@ -3,9 +3,11 @@ def find_binsearch(sorted_list: list, target):
     lower, upper = 0, len(sorted_list) - 1
     while lower <= upper:
         mid = (lower + upper) // 2
+
         if sorted_list[mid] == target:
             return mid
-        elif sorted_list[mid] < target:
+
+        if sorted_list[mid] < target:
             lower = mid + 1
         else:
             upper = mid - 1
@@ -16,18 +18,18 @@ def contains(haystack: list, needle, presorted=False):
 
     if presorted:
         return find_binsearch(haystack, needle) != -1
-    
+
     return needle in haystack
 
 def dict_from(data: list, separator=':'):
 
-    d = {}
+    dictified = {}
 
     for i in data:
         try:
             key, value = i.split(separator)
-            d[key] = value
+            dictified[key] = value
         except ValueError:
             pass
 
-    return d
+    return dictified
