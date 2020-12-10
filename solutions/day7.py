7527d7ad178f7b804197c24f6f331c2ddf4651c3
rds504 <8250802+rds504@users.noreply.github.com>
2020-12-10 07:36:49 +0000

    Day 7 tidy up

--- solutions/day7.py
+++ solutions/day7.py
@@ -1,16 +1,16 @@
 import re
-from tools.general import load_input
+from tools.general import load_input_list
 
-input_data = load_input("day7.txt").split('\n')
+input_data = load_input_list("day7.txt")
 
 def colours_containing(colour_map, colour):
 
     containing = set()
 
-    for i in colour_map:
-        if colour in colour_map[i]:
-            containing.add(i)
-            containing.update(colours_containing(colour_map, i))
+    for c in colour_map:
+        if colour in colour_map[c]:
+            containing.add(c)
+            containing.update(colours_containing(colour_map, c))
 
     return containing
 
@@ -19,15 +19,15 @@ def num_bags_contained(colour_map, colour):
     # Don't include this bag
     count = 0
 
-    for i in colour_map[colour]:
+    for b in colour_map[colour]:
         # Increment by 1 for every sub-bag, plus sub-sub-bags
-        count += colour_map[colour][i] * (1 + num_bags_contained(colour_map, i))
+        count += colour_map[colour][b] * (1 + num_bags_contained(colour_map, b))
 
     return count
 
-outer_pattern = re.compile("^([a-z]+ [a-z]+) bags contain ([0-9a-z ,]+).$")
-inner_pattern = re.compile("^([0-9]+) ([a-z]+ [a-z]+) bags?$")
-colour_map    = {}
+outer_pattern  = re.compile("^([a-z]+ [a-z]+) bags contain ([0-9a-z ,]+).$")
+inner_pattern  = re.compile("^([0-9]+) ([a-z]+ [a-z]+) bags?$")
+bag_colour_map = {}
 
 for rule in input_data:
     om = outer_pattern.match(rule)
@@ -37,7 +37,7 @@ for rule in input_data:
             im = inner_pattern.match(i)
             if im:
                 sub_map[im.group(2)] = int(im.group(1))
-        colour_map[om.group(1)] = sub_map
+        bag_colour_map[om.group(1)] = sub_map
 
-print(f"Part 1 => {len(colours_containing(colour_map, 'shiny gold'))}")
-print(f"Part 2 => {num_bags_contained(colour_map, 'shiny gold')}")
+print(f"Part 1 => {len(colours_containing(bag_colour_map, 'shiny gold'))}")
+print(f"Part 2 => {num_bags_contained(bag_colour_map, 'shiny gold')}")
