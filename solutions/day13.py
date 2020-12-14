38ce1aa2511a4bfa2bc594706bac3f6dd3955669
rds504 <8250802+rds504@users.noreply.github.com>
2020-12-14 12:18:12 +0000

    Tidied up a bit

--- solutions/day13.py
+++ solutions/day13.py
@@ -3,58 +3,55 @@ from math import inf
 from operator import mul
 from tools.general import load_input_list
 
-puzzle_input = load_input_list("day13.txt")
+def find_first_bus(earliest, buses):
 
-# Part 1
-earliest   = int(puzzle_input[0])
-first_bus  = None
-least_wait = inf
-for bus in puzzle_input[1].split(','):
-    if bus != 'x':
-        bus = int(bus)
+    first_bus, least_wait = None, inf
+
+    for bus in buses:
         wait = bus - (earliest % bus)
         if wait < least_wait:
-            first_bus  = bus
-            least_wait = wait
-print(f"Part 1 => {first_bus * least_wait}")
+            first_bus, least_wait = bus, wait
 
-# Part 2
+    return first_bus, least_wait
 
 # Extended Euclidean algorithm
-def xgcd(a, b):
+def xgcd(opr1, opr2):
 
     x0, x1 = 0, 1
     y0, y1 = 1, 0
 
-    while a != 0:
-        (q, a), b = divmod(b, a), a
+    while opr1 != 0:
+        (q, opr1), opr2 = divmod(opr2, opr1), opr1
         y0, y1 = y1, y0 - q * y1
         x0, x1 = x1, x0 - q * x1
 
-    return b, x0
+    return opr2, x0
 
-def mod_inverse(a, m):
+def mod_inverse(operand, modulus):
 
-    gcd, inv = xgcd(a, m)
+    gcd, inv = xgcd(operand, modulus)
 
     if gcd != 1:
-        raise ValueError(f"{a} and {m} are not coprime")
+        raise ValueError(f"{operand} and {modulus} are not coprime")
 
-    return inv % m
+    return inv % modulus
 
 # Chinese Remainder Theorem
-#   Given a sequence of pairs (c, m) such that 0 <= c < m and all m are pairwise coprime, then there
-#   is exactly one x satisfying 0 <= x < product(m) and x is congruent to c modulo m for all (c, m).
+#   Any sequence of pairs (c, m) where 0 <= c < m and all m are pairwise coprime defines a set of
+#   simultaneous linear congruences with exactly one solution x satisfying 0 <= x < product(m).
 def chinese_remainder(congruence_list):
 
-    prod = reduce(mul, (m for c, m in congruence_list))
-    rem  = 0
+    prod_m = reduce(mul, (m for c, m in congruence_list))
+    solution = 0
 
-    for c, m in congruences:
-        p = prod // m
-        rem = (rem + (m - c) * p * mod_inverse(p, m)) % prod
+    for c, m in congruence_list:
+        p = prod_m // m
+        solution = (solution + (m - c) * p * mod_inverse(p, m)) % prod_m
 
-    return rem
+    return solution
 
-congruences = [(i, int(bus)) for i, bus in enumerate(puzzle_input[1].split(',')) if bus != 'x']
-print(f"Part 2 => {chinese_remainder(congruences)}")
+earliest_dep, bus_schedule = load_input_list("day13.txt")
+bus_array = {i : int(bus) for i, bus in enumerate(bus_schedule.split(',')) if bus != 'x'}
+
+print(f"Part 1 => {reduce(mul, find_first_bus(int(earliest_dep), bus_array.values()))}")
+print(f"Part 2 => {chinese_remainder(bus_array.items())}")
